8a57d0d5b187e3276b17d038422fc2b4e542ac73
tommywood1101 <tommy.wood@gmail.com>
2026-07-24 10:54:07 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'somatic-reset-v2';
+const urlsToCache = [
+  './',
+  './index.html',
+  './app.html',
+  './manifest.json',
+  './watermarked_img_14225698465008636456.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
